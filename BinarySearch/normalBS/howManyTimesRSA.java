d3fe1fea85a5fda9fdbe8ed5ad092708ae532639
Siddharth_Kumar_Kothiyal <siddharthkothiyal30@gmail.com>
2025-10-02 20:01:17 +0530

    code modified

--- BinarySearch/normalBS/howManyTimesRSA.java
+++ BinarySearch/normalBS/howManyTimesRSA.java
@@ -1,55 +1,43 @@
 import java.util.ArrayList;
 
 public class howManyTimesRSA {
-
-    public static int bs(ArrayList<Integer> arr) {
-
-        int start = 0;
-        int end = arr.size() - 1;
+    public static int findKRotation(int[] arr) {
+        int start = 0, end = arr.length-1 ;
         int ans = Integer.MAX_VALUE;
+        int index = -1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
 
-        while (start < end) {
-              int mid = (start + end) / 2;
-
-              if(arr.get(start) <= arr.get(end)){
-                ans = Math.min(ans, start);
+            if (arr[start] <= arr[end]) {
+                if (arr[start] < ans) {
+                    index = start;
+                    ans = arr[start];
+                }
                 break;
-              }
+            }
 
-            //if left part is sorted:
-            if (arr.get(start) <= arr.get(mid)) {
-                // keep the minimum:
-                ans = Math.min(ans, arr.get(start));
+            if (arr[start] <= arr[mid]) {
 
-                // Eliminate left half:
+                if (arr[start] < ans) {
+                    index = start;
+                    ans = arr[start];
+                }
+
                 start = mid + 1;
+            } else {
+                if (arr[mid] < ans) {
+                    index = mid;
+                    ans = arr[mid];
+                }
 
-            } else { //if right part is sorted:
-
-                // keep the minimum:
-                ans = Math.min(ans, arr.get(mid));
-
-                // Eliminate right half:
                 end = mid - 1;
             }
         }
-        return ans;
-    }
-
-    public int findKRotation(ArrayList<Integer> nums) {
-        return bs(nums) ;
+        return index;
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(15);
-        arr.add(1);
-        arr.add(2);
-        arr.add(3);
-        arr.add(6);
-        arr.add(12);
-
-        System.out.println(bs(arr));
-
+        int[] arr = { 7,8,9,1,2,3,4,5,6 };
+        System.out.println(findKRotation(arr));
     }
 }
