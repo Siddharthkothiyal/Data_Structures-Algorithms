00af9a82d135577667a46c25563a305208bdc391
Siddharth_Kumar_Kothiyal <siddharthkothiyal30@gmail.com>
2025-12-15 20:00:10 +0530

    adding Question

---
+++ Arrays/kadaneAlgo.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class kadaneAlgo {
+    public static int maxSubArray(int[] nums) {
+        long sum = 0;
+        long max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return (int) max;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        System.out.println(maxSubArray(nums));
+
+    }
+}
---
+++ Arrays/majorityElem.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class majorityElem {
+      public static int majorityElement(int[] nums) {
+
+         Arrays.sort(nums);
+
+        return nums[nums.length/2];
+        
+    }
+
+    public static void main(String[] args) {
+
+        int nums[]= {2,2,1,1,1,2,2};
+
+        System.out.println(majorityElement(nums));
+    }
+}
