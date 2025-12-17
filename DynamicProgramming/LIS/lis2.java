7d19d6a9f59135c7f0b8afed2adecd811c355e4f
Siddharth_Kumar_Kothiyal <siddharthkothiyal30@gmail.com>
2025-12-17 20:10:56 +0530

    LIS

--- DynamicProgramming/LIS/lis2.java
+++ DynamicProgramming/LIS/lis2.java
@@ -1,5 +1,39 @@
 package DynamicProgramming.LIS;
 
+import java.util.Arrays;
+
 public class lis2 {
-    
+     public static int lis22(int[]nums , int index , int last , int [][] dp) {
+
+        if(index== nums.length) return 0;
+
+        if(dp[index][last+1] != -1) return dp[index][last+1] ;
+
+
+        int length = 0+ lis22(nums, index+1, last, dp);  //not take
+        if(last == -1 || nums[index] > nums[last]){
+            length  = Math.max(1+ lis22(nums, index+1, index, dp), length); //max of take or nottake
+            
+        }
+
+        return dp[index][last+1] = length;
+        
+    }
+
+     public static int lengthOfLISs(int[] nums) {
+
+        int [][] dp = new int[nums.length][nums.length+1];
+
+        for (int[] is : dp) {
+            Arrays.fill(is, -1);
+        }
+        return lis22(nums, 0, -1, dp);
+
+        
+    }
+public static void main(String[] args) {
+    int []nums = {10,9,2,5,3,7,101,18};
+    System.out.println(lengthOfLISs(nums));
+
+}
 }
