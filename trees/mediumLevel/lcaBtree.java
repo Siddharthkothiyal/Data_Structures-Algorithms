969625df8957008e8a012ad206b643764138a5ee
Siddharth_Kumar_Kothiyal <siddharthkothiyal30@gmail.com>
2026-01-07 19:48:32 +0530

    LCA

---
+++ trees/mediumLevel/lcaBtree.java
@@ -0,0 +1,24 @@
+package trees.mediumLevel;
+
+public class lcaBtree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+           if(root == null || root==p ||  root == q){
+        return root;
+       } 
+
+      TreeNode left= lowestCommonAncestor(root.left,  p,  q);
+      TreeNode  right= lowestCommonAncestor(root.right,  p,  q);
+
+       if(left== null) return right;
+       if(right== null) return left;
+
+       return root;
+    }
+    public static void main(String[] args) {
+
+
+        
+    }
+}
+
+
